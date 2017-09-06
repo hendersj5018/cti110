99eeba80155e25d1efc54dfa2416c662417b9a7d
hendersj5018 <31700892+hendersj5018@users.noreply.github.com>
2017-09-06 11:15:40 -0400

    Add files via upload

---
+++ m2t1_Henderson.py
@@ -0,0 +1,38 @@
+#cti 110
+#m2t1
+#8/30
+#Henderson
+
+#Suggested variables: totalsales, annualProfit
+
+#Get the user fot total sales
+totalsales = int(input( "total sales: "))
+#calculate the profit sales
+annualprofit = 0.23 * totalsales
+
+#Print the profit: amount
+print(annualprofit)
+
+
+
+
+
+
+
+
+
+'''
+#program to add two numbers
+#userTyped = input( "type a number: ")
+#userNumber= int(usertyped)
+
+# Get the numbers
+firstnumber = int(input("type a number: "))
+secondnumber = int(input("type another number: "))
+
+#add them
+answer= firstnumber + secondnumber
+
+#Print the result
+print("the answer is", answer)
+'''
